32e9e3b70d684ccca9f5d3b84d3175d1fc210207
Euan Gilmour <a9404253@9404253s-MacBook-Pro.local>
2019-05-29 16:17:07 +0100

    formatted header banner

---
+++ iou_app_frontend/src/components/MainPage/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+import './mainpage.css'
+
+const Header = props => {
+  return (
+    <div className='main-page-head'>
+      <img src='./images/top_banner.png' alt='top_banner' className='header-image'></img>
+    </div>
+  );
+}
+
+export default Header
