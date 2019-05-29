e974ce8f258edfa79e8d5b9439ef7cd2a267f8c5
Reka <a9886177@9886177s-MacBook-Pro.local>
2019-05-29 16:37:29 +0100

    back end updated with users, get, push routes work

--- iou_backend/routes/users.js
+++ iou_backend/routes/users.js
@@ -1,9 +1,23 @@
 var express = require('express');
 var router = express.Router();
+const MongoHelper = require('../db/mongo_helper.js')
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.post('/', function(req, res) {
+  MongoHelper.create('users', req.body).then(results => {
+    res.status(201).json('added user');
+  })
+});
+
+router.get('/', function(req, res) {
+  MongoHelper.get('users').then(result => {
+    res.status(200).json(result)
+  })
+});
+
+router.delete("/:id", function(req, res) {
+  MongoHelper.delete("users", req.params.id).then(results => {
+    res.status(204).json("User deleted");
+  });
 });
 
 module.exports = router;
