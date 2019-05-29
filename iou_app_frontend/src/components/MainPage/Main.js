d857890cc2cc070dde99224588358cb2c2ab1a8a
Euan Gilmour <a9404253@9404253s-MacBook-Pro.local>
2019-05-29 17:09:10 +0100

    some styling and dropdowns

--- iou_app_frontend/src/components/MainPage/Main.js
+++ iou_app_frontend/src/components/MainPage/Main.js
@@ -1,28 +1,40 @@
 import React from "react";
+import './mainpage.css'
 
 const Main = props => {
   console.log(props);
 
   const tasks = () => {
-    return <option>task</option>;
+    return (<li><a href="#">Babysitting</a></li>)
   }
 
   const users = () => {
-    return <option>user</option>;
+    return (<li><a href="#">Rose</a></li>);
   }
 
 
   return(
-  <div>
-
-      New Task:
-      <select>
-        {tasks}
-      </select>
-      <select>
-        {users}
-      </select>
-      <button>Submit</button>
+  <div className='dropdown-container'>
+    New Task:
+    <div className="dropdown">
+      <button id="dropdowns" className="btn btn-primary dropdown-toggle" type="button" data-toggle="dropdown">
+        Select Task
+        <span id="caret" className="caret"></span>
+      </button>
+      <ul className="dropdown-menu">
+        {tasks()}
+      </ul>
+    </div>
+    <div className="dropdown">
+      <button id="dropdowns" className="btn btn-primary dropdown-toggle" type="button" data-toggle="dropdown">
+        Select User
+        <span id="caret" className="caret"></span>
+      </button>
+      <ul className="dropdown-menu">
+        {users()}
+      </ul>
+    </div>
+    <button id='submitbutt' type="button" className="btn btn-primary">Submit</button>
   </div>
 )
 };
