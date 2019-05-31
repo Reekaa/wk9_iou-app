e809dea1fca0a3587647c9c5ec97a1967e328a4c
Matthew <a9852147@9852147s-MacBook-Pro.local>
2019-05-31 11:38:23 +0100

    minor changes

--- iou_app_frontend/src/components/MainPage/ViewGroupUsers.js
+++ iou_app_frontend/src/components/MainPage/ViewGroupUsers.js
@@ -3,21 +3,24 @@ import './mainpage.css'
 
 const ViewGroupUsers = (props) => {
 
-const groupUsersList = props.groupUsers.map((user, i) => {
-  return <div key={i} className='user-list-item'>
-           <p className='user-list-item-name'>
-            {user.name}
-          </p>
-          <p className='user-list-item-karma'>
-            {user.groups[0].karma}
-          </p>
-        </div>
-});
-
-console.log(props);
+  const groupUsersList = props.groupUsers.map((user, i) => {
+    return (
+      <div key={i} className='user-list-item'>
+        <p className='user-list-item-name'>
+          {user.name}
+        </p>
+        <p className='user-list-item-karma'>
+          {user.groups[0].karma}
+        </p>
+      </div>
+    )
+  });
 
   return (
     <div className='user-list'>
+      <div className='group-name'>
+        {props.groupName}
+      </div>
       {groupUsersList}
     </div>
   )
