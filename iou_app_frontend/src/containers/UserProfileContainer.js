817cba1e8f80df3cebb34d4d5b8364d9fd65506e
Euan Gilmour <a9404253@9404253s-MacBook-Pro.local>
2019-06-04 15:43:35 +0100

    fixed group users not loading

--- iou_app_frontend/src/containers/UserProfileContainer.js
+++ iou_app_frontend/src/containers/UserProfileContainer.js
@@ -1,7 +1,6 @@
 import { connect } from "react-redux";
 import UserProfile from "../components/UserProfilePage/UserProfile";
 
-console.log('UserProfile');
 
 const checkCurrentUser = (state) => {
   console.log();
