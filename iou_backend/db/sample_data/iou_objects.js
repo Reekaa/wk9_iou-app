5f0d04eb360f3cbbd0f7a2c6d8c99b05d3793337
Matthew <a9852147@9852147s-MacBook-Pro.local>
2019-06-05 23:49:12 +0100

    fix typo in data file

--- iou_backend/db/sample_data/iou_objects.js
+++ iou_backend/db/sample_data/iou_objects.js
@@ -14,6 +14,7 @@ db.users.insertMany(
     {
       "name": "Keith",
       "skill": "giving guitar lessons",
+      "userprofile": "The guitar is my big passion in life and I love helping people learn to play.",
       "groups": [
         {
           "groupName": "SW2",
@@ -27,6 +28,7 @@ db.users.insertMany(
     {
       "name": "Ben",
       "skill": "giving swimming lessons",
+      "userprofile": "If you want to improve yuor swimming I can help you out. Just don't ask to borrow my GB athletics t-shirt!",
       "groups": [
         {
           "groupName": "SW2",
@@ -51,6 +53,7 @@ db.users.insertMany(
     {
       "name": "Reka",
       "skill": "dog walking",
+      "userprofile": "When I'm not coding, I love going out for walks. I can take your dog with me!",
       "groups": [
         {
           "groupName": "SW2",
@@ -79,6 +82,7 @@ db.users.insertMany(
     {
       "name": "Euan",
       "skill": "cleaning windows",
+      "userprofile": "I'm pretty tall, so cleaning windows is no hassle. Happy to help any time!",
       "groups": [
         {
           "groupName": "SW2",
@@ -92,6 +96,7 @@ db.users.insertMany(
     {
       "name": "Rose",
       "skill": "babysitting",
+      "userprofile": "Kids love me and my friends say I'm really responsible. I'd be happy to do some babnysitting for you.",
       "groups": [
         {
           "groupName": "SW2",
@@ -116,6 +121,7 @@ db.users.insertMany(
     {
       "name": "Hamish",
       "skill": "painting and decorating",
+      "userprofile": "In a past life I was an artist so I enjoy getting out a paint brush, even if it's only to paint a wall.",
       "groups": [
         {
           "groupName": "SW2",
@@ -141,6 +147,7 @@ db.users.insertMany(
     {
       "name": "Rod",
       "skill": "photographing events",
+      "userprofile": "I'm a keen photographer and I've photographed a few weddings in my time. I can make you an album you'll keep forever.",
       "groups": [
         {
           "groupName": "SW2",
@@ -154,6 +161,7 @@ db.users.insertMany(
     {
       "name": "Pawel",
       "skill": "providing spiritual support",
+      "userprofile": "It's not always easy to keep life simple, but if you need some support I'm here to help.",
       "groups": [
         {
           "groupName": "SW2",
@@ -167,6 +175,7 @@ db.users.insertMany(
     {
       "name": "James",
       "skill": "picking up people's shopping",
+      "userprofile": "It can be a real pain to pick up your weekly shop, so I like to help out by collecting shopping for my friends.",
       "groups": [
         {
           "groupName": "SW2",
@@ -180,21 +189,20 @@ db.users.insertMany(
     {
       "name": "Kris",
       "skill": "giving piano lessons",
+      "userprofile": "The piano is a great starting point for learning any musical instrument. If you'd like a few lessons, let me know.",
       "groups": [
         {
           "groupName": "SW2",
           "karma": 20,
           "completedTasks": [
             {
-              {
-                "task": "Cleaning",
-                "value": 10,
-                "whoFor": "Rod",
-                "karma": 20,
-                "date": "2019-06-02T20:56:56.526Z",
-                "cost": {
-                  "hours": 2
-                }
+              "task": "Cleaning",
+              "value": 10,
+              "whoFor": "Rod",
+              "karma": 20,
+              "date": "2019-06-02T20:56:56.526Z",
+              "cost": {
+                "hours": 2
               }
             }
           ]
@@ -206,6 +214,7 @@ db.users.insertMany(
     {
       "name": "Sian",
       "skill": "proofreading",
+      "userprofile": "I love writing, and since I love reading too I'd be happy to help proofread your work.",
       "groups": [
         {
           "groupName": "SW2",
